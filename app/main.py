5434b0fc9a5060268541d9b0ad72f4a17f63cc37
Balintbelavari <balintbelavari.bb@gmail.com>
2025-04-01 11:55:31 +0200

    git conflict resolved

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ app = FastAPI()
 # ✅ Enable CORS for React frontend (localhost:3000 for development)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://lc-security-backend-d51e9de3f86b.herokuapp.com/", "http://127.0.0.1:8001"],  # React dev server, heroku hosting, fastapi local
+    allow_origins=["http://localhost:3000", "https://lc-security-backend-d51e9de3f86b.herokuapp.com/", "http://127.0.0.1:8001/"],  # React dev server, heroku hosting, fastapi local
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
