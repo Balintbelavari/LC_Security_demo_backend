cc1504da06525d4bb571d8e39974e2b34b499679
Ahed Hatter <132410904+Ahedhatter@users.noreply.github.com>
2025-10-01 16:48:18 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -63,7 +63,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000", 
-        "https://lc-security-backend-d51e9de3f86b.herokuapp.com",  # ✅ No trailing slash
+        "https://lc-security-backend-d51e9de3f86b.herokuapp.com/predict",  # ✅ No trailing slash
         "http://127.0.0.1:8001", 
         "https://demo.lcsecurity.ai",  # ✅ No trailing slash
         "https://demo-lcsecurity.lovable.app",  # ✅ No trailing slash
